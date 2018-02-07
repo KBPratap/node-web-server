0a54da975c7820082a489705544480e69a010cf7
bhanuka <bhanu.karumuri@citrix.com>
2018-02-06 21:22:20 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+
+const express = require('express');
+const hbs = require('hbs');
+
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (message) => {
+    return message.toUpperCase();
+})
+
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>')
+    // res.send({
+    //     name: 'Bhanu',
+    //     likes: [
+    //         'Biking',
+    //         'Painting'
+    //     ]
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Aloha!'
+    })
+
+})
+
+app.get('/about', (req, res) => {
+    // res.send('About Page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    })
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
